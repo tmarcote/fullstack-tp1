85efc281a970ab03eaf7fad04151c2123795813e
Tomas Santiago Marcote <tomas.marcote@gmail.com>
2021-06-02 17:19:10 -0300

    added untracked admin files

---
+++ frontend/src/layouts/AdminLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Admin Page
+        </q-toolbar-title>
+
+        <div>
+          {{ username }}
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Menu
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Usuarios',
+    caption: 'Administrar usuarios',
+    icon: 'supervisor_account',
+    link: '/#/admin/usuarios'
+  },
+  {
+    title: 'Productos',
+    caption: 'Administrar productos',
+    icon: 'add_shopping_cart',
+    link: '/#/admin/productos'
+  },
+  {
+    title: 'Reporte de Ventas',
+    caption: 'Reporte de Ventas',
+    icon: 'analytics',
+    link: '/#/admin/reportes/ventas'
+  }
+]
+
+export default {
+  name: 'AdminLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  },
+  computed: {
+    username: function () {
+      return this.$store.state.user.username
+    }
+  }
+}
+</script>
---
+++ frontend/src/pages/Admin.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>Admin Page</h3>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'PageAdmin'
+}
+</script>
